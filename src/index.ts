6a5ecfe7aacabf37ceae7846e28afd13288fb0f0
mwx0621 <mwx0621@gmail.com>
2021-08-17 21:34:24 +0800

    add Tag to documnet

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import './styles/index.scss';
 export { default as Button } from './components/Button';
 
 export { default as Tabs } from './components/Tabs';
+
+export { default as Tag } from './components/Tag';
